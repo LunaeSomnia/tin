1a538c99db9959473c84f4a2ab29aa4f9fe03ef9
Lunae Somnia <lunaesomnia@protonmail.com>
2022-08-07 20:56:03 +0200

    Import font file

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ mod file;
 use app::{AppEvent, AppState};
 use vizia::prelude::*;
 
+const TIN_ICONS: &[u8] = include_bytes!("../static/TinIcons.otf");
+
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     Application::new(|cx| {
         cx.add_stylesheet("static/styles.css")
             .expect("Failed to find file");
+
+        cx.add_font_mem("TinIcons", TIN_ICONS);
 
         AppState::default().build(cx);
 
@@ -19,6 +23,10 @@ fn main() {
                 |cx| Label::new(cx, "Press me"),
             );
 
+            // TODO: Improve the font file
+            Label::new(cx, "ABC").font("TinIcons").font_size(18.0);
+            Label::new(cx, "abc").font("TinIcons").font_size(14.0);
+
             Binding::new(cx, AppState::file, move |cx, file| {
                 if let Some(f) = file.get(cx) {
                     Label::new(cx, &f.data);
@@ -27,6 +35,6 @@ fn main() {
         });
     })
     .title("Tin")
-    .inner_size((450, 600))
+    .inner_size((1000, 800))
     .run();
 }
